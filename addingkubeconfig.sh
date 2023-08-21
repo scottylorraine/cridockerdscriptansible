17028b81253184ac4da38de4b6664527483e6dd3
tony <111169781+scottylorraine@users.noreply.github.com>
2023-08-22 03:41:24 +0530

    Update addingkubeconfig.sh
    also add the pod network plugin for the kubeernets cluster

--- addingkubeconfig.sh
+++ addingkubeconfig.sh
@@ -5,3 +5,7 @@ cd /home
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
