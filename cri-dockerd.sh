f8a9132df93c1120858f80f796b988d117346835
tony <111169781+scottylorraine@users.noreply.github.com>
2023-08-21 18:22:29 +0530

    Update cri-dockerd.sh

--- cri-dockerd.sh
+++ cri-dockerd.sh
@@ -1,8 +1,16 @@
-cp -r cri-dockerd /usr/local/bin/
-chmod -R 0755 /usr/local/bin/cri-dockerd
-chown -R root:root /usr/local/bin/cri-dockerd
-cd /root/cri-dockerd/
-install packaging/systemd/* /etc/systemd/system
+wget https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+cd /root/cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
 sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
 systemctl daemon-reload
+systemctl enable cri-docker.service
 systemctl enable --now cri-docker.socket
+systemctl start cri-docker.service 
+systemctl start --now cri-docker.socket
