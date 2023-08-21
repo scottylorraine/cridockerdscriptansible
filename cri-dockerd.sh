ece77b127dfb48c3f31f4bdd15fe0ba45cd6a00b
tony <111169781+scottylorraine@users.noreply.github.com>
2023-08-21 18:44:30 +0530

    Update cri-dockerd.sh

--- cri-dockerd.sh
+++ cri-dockerd.sh
@@ -1,16 +1,28 @@
+                                             
+#!/bin/bash
+
+# Download and install Go 1.20.5
 wget https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
+
+# Navigate to the cri-dockerd directory and build it
 cd /root/cri-dockerd
-mkdir bin
+mkdir -p bin
 go build -o bin/cri-dockerd
-mkdir -p /usr/local/bin
-install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
-cp -a packaging/systemd/* /etc/systemd/system
-sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
-systemctl daemon-reload
-systemctl enable cri-docker.service
-systemctl enable --now cri-docker.socket
-systemctl start cri-docker.service 
-systemctl start --now cri-docker.socket
+
+# Install cri-dockerd
+sudo mkdir -p /usr/local/bin
+sudo install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+
+# Copy systemd files and modify the cri-docker.service file
+sudo cp -a packaging/systemd/* /etc/systemd/system
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+
+# Reload systemd, enable and start the service and socket
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+sudo systemctl start cri-docker.service 
+sudo systemctl start --now cri-docker.socket
