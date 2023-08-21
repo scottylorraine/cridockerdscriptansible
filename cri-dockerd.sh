78ff1034f19ce534e197398efc26e3065dc094ec
tony <111169781+scottylorraine@users.noreply.github.com>
2023-08-22 02:02:35 +0530

    Update cri-dockerd.sh

--- cri-dockerd.sh
+++ cri-dockerd.sh
@@ -12,17 +12,4 @@ cd /root/cri-dockerd
 mkdir -p bin
 go build -o bin/cri-dockerd
 
-# Install cri-dockerd
-sudo mkdir -p /usr/local/bin
-sudo install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
 
-# Copy systemd files and modify the cri-docker.service file
-sudo cp -a packaging/systemd/* /etc/systemd/system
-sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
-
-# Reload systemd, enable and start the service and socket
-sudo systemctl daemon-reload
-sudo systemctl enable cri-docker.service
-sudo systemctl enable --now cri-docker.socket
-sudo systemctl start cri-docker.service 
-sudo systemctl start --now cri-docker.socket
